2bf9350a3f041014ce0e501e75bae3d2e3e3e598
James Andrews <jamesftw@sfs-wifi-dhcp-10-143-246-141.wifi.sfsu.edu>
2016-09-29 13:03:20 -0700

    added reverse.js

--- reverse.js
+++ reverse.js
@@ -4,21 +4,37 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 
-//HTTP POST
-fetch('http://challenge.code2040.org/api/reverse', {
-  method: 'POST',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    token: '3af275cbf4eb4d719c98a391f7f50854',
-    github: 'https://github.com/JamesFTW/code2040',
+//function to reverse string
+function reverse_string(string){
+  return string.split('').reverse().join('')
+}
+
+//Connect to first end_point
+ fetch('http://challenge.code2040.org/api/reverse',{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        token: '3af275cbf4eb4d719c98a391f7f50854',
+      })
+    }).then(function(response){
+      return response.text() //return endpoint string
+}).then(function(response_text){
+
+//Connect to second end_point and validate
+  fetch('http://challenge.code2040.org/api/reverse/validate',{
+       method: 'POST',
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json'
+       },
+       body: JSON.stringify({
+         token: '3af275cbf4eb4d719c98a391f7f50854',
+         string: reverse_string(response_text),
+       })
+}).then(function(response){
+  console.log(response)
   })
 })
-
-//reverse a string
-
-  function reverse_string (string){
-    return string.split("").reverse().join("")
-  }
